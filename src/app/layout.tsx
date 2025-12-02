faf71c66d4f618b070333a58abc8c352f16cf27b
legaz <188694281+Legaz-le@users.noreply.github.com>
2025-12-02 22:15:35 +0900

    added ShadCn

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from "next";
+import { Inter } from "next/font/google";
 import "./globals.css";
+import { cn } from "@/lib/utils";
 
+const inter = Inter({ subsets: ["latin"], variables: "--font-sans" });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -15,6 +18,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
+        className={cn( "bg-background min-h-screen font-sans antialiased",
+          inter.variables
+        )}
       >
         {children}
       </body>
