352226bca72135ef6e4b303281ea815580ec15e4
legaz <188694281+Legaz-le@users.noreply.github.com>
2025-12-12 23:42:15 +0900

    added Stripe payment to website

---
+++ src/app/(customerFacing)/products/[id]/purchase/page.tsx
@@ -0,0 +1,33 @@
+import prisma from "@/lib/prisma";
+import { notFound } from "next/navigation";
+import Stripe from "stripe";
+import { CheckoutForm } from "./_components/CheckoutForm";
+
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string);
+
+export default async function PurchasePage({
+  params,
+}: {
+  params: Promise<{ id: string }>;
+}) {
+  const { id } = await params;
+  const product = await prisma.product.findUnique({ where: { id } });
+  if (product == null) return notFound();
+
+  const paymentIntents = await stripe.paymentIntents.create({
+    amount: product.priceInCents,
+    currency: "USD",
+    metadata: { productId: product.id },
+  });
+
+  if (paymentIntents.client_secret === null) {
+    throw Error("Stripe failed to create payment intent");
+  }
+
+  return (
+    <CheckoutForm
+      product={product}
+      clientSecret={paymentIntents.client_secret}
+    />
+  );
+}
