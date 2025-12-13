9c17ae60cc2333fa598d8bd45caa7dcb575e8718
legaz <188694281+Legaz-le@users.noreply.github.com>
2025-12-13 18:33:56 +0900

    fixed bug with not able to download

--- src/app/(customerFacing)/products/download/[downloadVerificationId]/route.ts
+++ src/app/(customerFacing)/products/download/[downloadVerificationId]/route.ts
@@ -1,17 +1,18 @@
 import prisma from "@/lib/prisma";
 import { NextRequest, NextResponse } from "next/server";
-import fs from "fs/promises"
+import fs from "fs/promises";
 
 export async function GET(
   req: NextRequest,
-  {
-    params: { downloadVerificationId },
-  }: { params: { downloadVerificationId: string } }
+  { params }: { params: Promise<{ downloadVerificationId: string }> }
 ) {
+  const { downloadVerificationId } = await params;
+  
   const data = await prisma.downloadVerification.findUnique({
     where: { id: downloadVerificationId, expiresAt: { gt: new Date() } },
     select: { product: { select: { filePath: true, name: true } } },
   });
+
   if (data == null) {
     return NextResponse.redirect(
       new URL("/products/download/expired", req.url)
