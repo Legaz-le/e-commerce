c8f7e6feb7005a61c45a0727ac1dac8fc4fc514b
legaz <188694281+Legaz-le@users.noreply.github.com>
2025-12-13 17:26:47 +0900

    made download page after purchase

--- src/app/(customerFacing)/stripe/purchase-success/page.tsx
+++ src/app/(customerFacing)/stripe/purchase-success/page.tsx
@@ -14,9 +14,7 @@ export default async function SuccessPage({
   searchParams: Promise<{ payment_intent: string }>;
 }) {
   const { payment_intent } = await searchParams;
-  const paymentIntent = await stripe.paymentIntents.retrieve(
-    payment_intent
-  );
+  const paymentIntent = await stripe.paymentIntents.retrieve(payment_intent);
 
   if (paymentIntent.metadata.productId == null) return notFound();
 
@@ -50,7 +48,11 @@ export default async function SuccessPage({
           </div>
           <Button className="mt-4" size="lg" asChild>
             {isSuccess ? (
-              <a></a>
+              <a
+                href={`/products/download/${await createDownloadVerification(
+                  product.id
+                )}`}
+              >Download</a>
             ) : (
               <Link href={`/products/${product.id}/purchase`}>Try Again</Link>
             )}
@@ -59,4 +61,15 @@ export default async function SuccessPage({
       </div>
     </div>
   );
+}
+
+async function createDownloadVerification(productId: string) {
+  return (
+    await prisma.downloadVerification.create({
+      data: {
+        productId,
+        expiresAt: new Date(Date.now() + 1000 * 60 * 60 * 24),
+      },
+    })
+  ).id;
 }
