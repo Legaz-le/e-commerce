75e2f09f40f395af7ad7c51dc31c43b08a6d3121
legaz <188694281+Legaz-le@users.noreply.github.com>
2025-12-25 02:35:26 +0900

    clean data

--- src/app/(customerFacing)/_components/Hero.tsx
+++ src/app/(customerFacing)/_components/Hero.tsx
@@ -1,4 +1,6 @@
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
+import Link from "next/link";
 
 export function Hero() {
   return (
@@ -6,14 +8,18 @@ export function Hero() {
       <div className="flex-1 mt-15 relative">
         <div className="px-15 flex flex-col">
           <div className="space-y-10 w-[513px] ">
-          <h1 className="text-white text-3xl font-mono">
-            The furniture brand for the future, with  timeless design
-          </h1>
-          <a className="text-white bg-gray-700 px-5 py-2 ">View Collections</a>
+            <h1 className="text-white text-3xl font-mono">
+              The furniture brand for the future, with timeless design
+            </h1>
+            <Button className="text-white bg-gray-700 px-5 py-5 cursor-pointer">
+              <Link href="/products"> View Collections</Link>
+            </Button>
           </div>
-          <p className="text-white absolute bottom-15 text-lg w-[602px]">A new era in eco friendly furniture with Avelon, the French luxury retail brand
-          with nice fonts, tasteful colors and a beautiful way to display things digitally 
-          using modern web technologies.</p>
+          <p className="text-white absolute bottom-15 text-lg w-[602px]">
+            A new era in eco friendly furniture with Avelon, the French luxury
+            retail brand with nice fonts, tasteful colors and a beautiful way to
+            display things digitally using modern web technologies.
+          </p>
         </div>
       </div>
       <Image
--- src/app/(customerFacing)/layout.tsx
+++ src/app/(customerFacing)/layout.tsx
@@ -12,7 +12,7 @@ export default function Layout({
     <>
       <div className="space-y-5">
         <TopNavbar />
-        <div className="flex border-b border-solid"/>
+        <div className="flex border-b border-solid" />
         <div className="container mx-auto">
           <Nav>
             <NavLink href="/">Plant pots</NavLink>
