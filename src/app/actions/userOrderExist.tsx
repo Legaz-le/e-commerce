ae09c2923a0c0401cb18ce2518ddbac491e75a28
legaz <188694281+Legaz-le@users.noreply.github.com>
2026-01-04 20:03:00 +0900

    fixed bug with order

--- src/app/actions/userOrderExist.tsx
+++ src/app/actions/userOrderExist.tsx
@@ -5,7 +5,7 @@ import prisma from "@/lib/prisma";
 export async function userOrderExist(email: string, productId: string) {
   return (
     (await prisma.order.findFirst({
-      where: { user: { email }, productId },
+      where: { user: { email }, items: { some: { productId } } },
       select: { id: true },
     })) != null
   );
