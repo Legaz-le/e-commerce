74daa770429b4440b78d304144cbf8842b6aa12d
legaz <188694281+Legaz-le@users.noreply.github.com>
2026-01-11 21:11:52 +0900

    made merege system

--- src/components/CartMerge.tsx
+++ src/components/CartMerge.tsx
@@ -1,3 +1,34 @@
+"use client";
+import { useRef, useEffect } from "react";
+import { useAuth } from "@clerk/nextjs";
+import { mergeGuestCart } from "@/actions/mergeCart";
+
 export function CartMerge() {
-  
+  const { isSignedIn } = useAuth();
+  const ref = useRef(false);
+
+  useEffect(() => {
+    if (ref.current) return;
+
+    async function check() {
+      if (!isSignedIn) return;
+
+      const stored = localStorage.getItem("guestCart");
+
+      if (!stored) return;
+
+      const item = JSON.parse(stored);
+
+      if (item.length === 0) return;
+
+      const result = await mergeGuestCart(item);
+
+      if (result.success) {
+        localStorage.removeItem("guestCart");
+      }
+      ref.current = true;
+    }
+    check();
+  }, [isSignedIn]);
+  return null
 }
