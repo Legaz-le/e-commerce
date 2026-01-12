b18a9a1a29381f550de59f20f8f4cae8d865dc67
legaz <188694281+Legaz-le@users.noreply.github.com>
2026-01-13 01:45:01 +0900

    made checkout page to able to purchase

--- src/app/(customerFacing)/checkout/page.tsx
+++ src/app/(customerFacing)/checkout/page.tsx
@@ -1,6 +1,73 @@
 import { auth } from "@clerk/nextjs/server";
+import prisma from "@/lib/prisma";
+import { redirect } from "next/navigation";
+import Stripe from "stripe";
+import { CheckoutForm } from "@/components/CheckoutForm";
 
 export default async function CheckoutPage() {
   const { userId: clerkId } = await auth();
-  
+
+  if (!clerkId) redirect("/sign-in?redirect_url=/checkout");
+
+  const user = await prisma.user.findUnique({ where: { clerkId: clerkId } });
+
+  if (!user) redirect("/sign-up");
+
+  const cartItems = await prisma.cartItem.findMany({
+    where: {
+      cart: { userId: user.id },
+      product: {
+        isAvailableForPurchase: true,
+      },
+    },
+    include: {
+      product: {
+        select: {
+          id: true,
+          name: true,
+          priceInCents: true,
+          imagePath: true,
+          description: true,
+        },
+      },
+    },
+  });
+
+  if (cartItems.length === 0) redirect("/basket");
+
+  const totalInCents = cartItems.reduce(
+    (sum, item) => sum + item.product.priceInCents * item.quantity,
+    0,
+  );
+
+  const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string);
+
+  const paymentIntent = await stripe.paymentIntents.create({
+    amount: totalInCents,
+    currency: "usd",
+    metadata: {
+      userId: user.id,
+      cartCheckout: "true",
+      cartItems: JSON.stringify(
+        cartItems.map((item) => ({
+          productId: item.product.id,
+          quantity: item.quantity,
+          priceInCents: item.product.priceInCents,
+        })),
+      ),
+    },
+  });
+
+  if (!paymentIntent.client_secret) {
+    throw new Error("client_secret not existing");
+  }
+  return (
+    <div>
+      <CheckoutForm
+        cartItems={cartItems}
+        totalInCents={totalInCents}
+        clientSecret={paymentIntent.client_secret}
+      />
+    </div>
+  );
 }
