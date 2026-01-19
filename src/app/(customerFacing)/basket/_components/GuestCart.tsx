1ff8bc535397b0dcedb95a68e61cb057beef47df
legaz <188694281+Legaz-le@users.noreply.github.com>
2026-01-19 15:31:36 +0900

    forget added cartItem

--- src/app/(customerFacing)/basket/_components/GuestCart.tsx
+++ src/app/(customerFacing)/basket/_components/GuestCart.tsx
@@ -56,6 +56,7 @@ export function GuestCart() {
 
                 return (
                   <ProductCardBasket
+                    cartItemId={item.id}
                     key={item.id}
                     {...item}
                     quantity={quantity}
