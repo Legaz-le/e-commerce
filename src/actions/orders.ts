cf0fa4c68241eabe05b4e95dbc58abf5c2d20aa7
legaz <188694281+Legaz-le@users.noreply.github.com>
2026-01-21 15:28:23 +0900

    changed orders logic for displaying order history instead of email sender

--- src/actions/orders.ts
+++ src/actions/orders.ts
@@ -1,28 +1,18 @@
 "use server";
 
-import OrderHistoryEmail from "@/app/email/OrderHistory";
 import prisma from "@/lib/prisma";
-import { Resend } from "resend";
-import z from "zod";
-import { render } from "@react-email/render";
+import { auth } from "@clerk/nextjs/server";
 
-const emailSchema = z.email();
-const resend = new Resend(process.env.RESEND_API_KEY as string);
+export async function getOrderForUser() {
+  const { userId: clerkId } = await auth();
 
-export async function emailOrderHistory(
-  prevState: unknown,
-  formData: FormData,
-): Promise<{ message?: string; error?: string }> {
-  const result = emailSchema.safeParse(formData.get("email"));
-  if (result.success === false) {
-    return { error: "Invalid email address" };
-  }
+  if (!clerkId) return { orders: [], error: "Not authenticated" };
 
   const user = await prisma.user.findUnique({
-    where: { email: result.data },
+    where: { clerkId: clerkId },
     select: {
-      email: true,
       orders: {
+        orderBy: { createdAt: "desc" },
         select: {
           totalPaidInCents: true,
           id: true,
@@ -47,66 +37,7 @@ export async function emailOrderHistory(
     },
   });
 
-  if (user == null) {
-    return {
-      message:
-        "Check your email to view your order history and download your products",
-    };
-  }
+  if (!user) return { orders: [], error: "User not found" };
 
-  const orders = await Promise.all(
-    user.orders.map(async (order) => {
-      const items = await Promise.all(
-        order.items.map(async (item) => {
-          const downloadVerification =
-            await prisma.downloadVerification.create({
-              data: {
-                expiresAt: new Date(Date.now() + 24 * 60 * 60 * 1000),
-                productId: item.product.id,
-              },
-            });
-  
-          const baseUrl =
-            process.env.NEXT_PUBLIC_SERVER_URL || "http://localhost:3000";
-  
-          return {
-            product: {
-              name: item.product.name,
-              imagePath: `${baseUrl}${item.product.imagePath}`,
-              description: item.product.description,
-            },
-            quantity: item.quantity,
-            priceInCents: item.priceInCents,
-            downloadVerificationId: `${baseUrl}/products/download/${downloadVerification.id}`,
-          };
-        })
-      );
-  
-      return {
-        id: order.id,
-        totalPaidInCents: order.totalPaidInCents,
-        createdAt: order.createdAt,
-        items,
-      };
-    })
-  );
-
-
-  const data = await resend.emails.send({
-    from: `Support <onboarding@resend.dev>`,
-    to: user.email,
-    subject: "Order History",
-    html: await render(OrderHistoryEmail({ orders })),
-  });
-
-  if (data.error) {
-    return {
-      error: "There was an error sending your email. Please try again,",
-    };
-  }
-
-  return {
-    message:
-      "Check your email to view your order history and download your products",
-  };
+  return { orders: user.orders, error: null };
 }
