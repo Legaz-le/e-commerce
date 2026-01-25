970877b25fe501605976f03b9238eafe97e97a93
legaz <188694281+Legaz-le@users.noreply.github.com>
2026-01-25 23:18:14 +0900

    made same design as login cart page

--- src/app/(customerFacing)/basket/_components/GuestCart.tsx
+++ src/app/(customerFacing)/basket/_components/GuestCart.tsx
@@ -15,7 +15,7 @@ type Product = {
 };
 
 export function GuestCart() {
-  const items =  useCartStore((state) => state.items);
+  const items = useCartStore((state) => state.items);
   const [products, setProducts] = useState<Product[]>([]);
   const [loading, setIsLoading] = useState(true);
 
@@ -40,39 +40,52 @@ export function GuestCart() {
     return sum + product.priceInCents * quantity;
   }, 0);
 
-  return (
-    <div>
-      {loading ? (
-        "Loading..."
-      ) : (
-        <div>
-          {products.length === 0 ? (
-            <p>Your cart is empty</p>
-          ) : (
-            <>
-              {products.map((item) => {
-                const carItem = items.find(
-                  (cart) => cart.productId === item.id,
-                );
-                const quantity = carItem?.quantity || 0;
+  if (loading) {
+    return <p>Loading...</p>;
+  }
 
-                return (
-                  <ProductCardBasket
-                    cartItemId={item.id}
-                    key={item.id}
-                    {...item}
-                    quantity={quantity}
-                  />
-                );
-              })}
-              <div className="text-right space-y-5">
-                <div>Subtotal: {formatCurrency(subtotal / 100)}</div>
-                <CheckoutButton />
-              </div>
-            </>
-          )}
+  return (
+    <>
+      {products.length === 0 ? (
+        <p>Your cart is empty</p>
+      ) : (
+        <div className="flex flex-col w-full space-y-5">
+          <div className="grid grid-cols-[2fr_1fr_1fr] gap-4 w-full font-semibold">
+            <h1>Product</h1>
+            <h2 className="text-center">Quantity</h2>
+            <h3 className="text-right">Total</h3>
+          </div>
+          <div className="hidden md:flex border-b border-solid" />
+          <div className="space-y-4">
+            {products.map((item) => {
+              const cartItem = items.find((cart) => cart.productId === item.id);
+              const quantity = cartItem?.quantity || 0;
+
+              return (
+                <ProductCardBasket
+                  cartItemId={item.id}
+                  key={item.id}
+                  {...item}
+                  quantity={quantity}
+                />
+              );
+            })}
+          </div>
+          <div className="hidden md:flex border-b border-solid" />
+          <div className="self-end text-end space-y-3 mt-4">
+            <div className="flex justify-end space-x-3">
+              <span className="text-lg">Subtotal</span>
+              <span className="text-lg font-semibold">
+                {formatCurrency(subtotal / 100)}
+              </span>
+            </div>
+            <p className="text-sm text-gray-600">
+              Taxes and shipping are calculated at checkout
+            </p>
+            <CheckoutButton />
+          </div>
         </div>
       )}
-    </div>
+    </>
   );
 }
