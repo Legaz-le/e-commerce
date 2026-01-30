2715189b86089845967a6ee9907be055724957af
legaz <188694281+Legaz-le@users.noreply.github.com>
2026-01-30 16:35:21 +0900

    updated Icons

--- src/app/(customerFacing)/_components/TopNavbar.tsx
+++ src/app/(customerFacing)/_components/TopNavbar.tsx
@@ -1,11 +1,10 @@
 "use client";
 
-import Image from "next/image";
 import { NavLink } from "./Navbar";
 import Link from "next/link";
 import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
 import { MobileMenu } from "./MobileMenu";
-import { ShoppingBag } from "lucide-react";
+import { Search, ShoppingCart, ShoppingBag, User } from "lucide-react";
 import { useCartStore } from "@/store/cartStore";
 import { useAuth } from "@clerk/nextjs";
 import { useEffect } from "react";
@@ -27,41 +26,32 @@ export function TopNavbar() {
   return (
     <div className=" px-15">
       <div className="flex items-center justify-between mt-5">
-        <Link href="/search">
-          <Image
-            src="/images/Search.png"
-            alt="Search"
-            width="20"
-            height="20"
-            className="cursor-pointer"
+        <Link href="/search" aria-label="Search">
+          <Search
+            size={20}
+            className="cursor-pointer text-brand hover:text-brand-primary transition-colors"
           />
         </Link>
         <NavLink href="/">
           <h1 className="text-2xl font-medium">Avion</h1>
         </NavLink>
         <div className="sm:flex space-x-5 hidden ">
-          <Link href="/basket" className="relative">
-            <Image
-              src="/images/Shopping--cart.png"
-              alt="Shopping Cart"
-              width={20}
-              height={20}
-              className="cursor-pointer"
+          <Link href="/basket" className="relative" aria-label="Shopping Cart">
+            <ShoppingCart
+              size={20}
+              className="cursor-pointer text-brand hover:text-brand-primary transition-colors"
             />
             {cartCount > 0 && (
-              <span className="absolute -top-2 -right-2 bg-brand text-white text-sm w-4 h-4 flex items-center justify-center rounded-full">
+              <span className="absolute -top-2 -right-2 bg-brand text-white text-xs w-4 h-4 flex items-center justify-center rounded-full">
                 {cartCount}
               </span>
             )}
           </Link>
           <SignedOut>
             <SignInButton>
-              <Image
-                alt="Sign in"
-                src="/images/User--avatar.png"
-                width={20}
-                height={20}
-                className="cursor-pointer"
+              <User
+                size={20}
+                className="cursor-pointer text-brand hover:text-brand-primary transition-colors"
               />
             </SignInButton>
           </SignedOut>
